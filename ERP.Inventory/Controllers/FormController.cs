fa636bcc9f6e6eb0a12d97f126186540c1192d92
Kennedy Sagarino <khean92@gmail.com>
2020-02-03 21:49:07 +0800

    edit UI

--- ERP.Inventory/Controllers/FormController.cs
+++ ERP.Inventory/Controllers/FormController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rotativa;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,9 +17,33 @@ namespace ERP.Inventory.Controllers
 
         public ActionResult FormView(string pageName, string itemType)
         {
+            ViewBag.Report = false;
+
             ViewBag.ItemType = itemType;
 
             return PartialView($"~/Views/Form/_{pageName}.cshtml");
+        }
+
+        public ActionResult FormReport(string pageName, string itemType)
+        {
+            ViewBag.Report = true;
+
+            ViewBag.ItemType = itemType;
+
+            return View($"~/Views/Form/_{pageName}.cshtml");
+
+        }
+        public ActionResult SampleReport(string pageName, string itemType)
+        {
+            //Response.AppendHeader("Content-Disposition", new System.Net.Mime.ContentDisposition { Inline = true, }.ToString());
+
+            var data = new ActionAsPdf("FormReport", new { pageName = pageName, itemType = itemType })
+            {
+                PageSize = Rotativa.Options.Size.A4,
+                PageMargins = { Left = 10, Bottom = 10, Right = 10, Top = 10 },
+            };
+
+            return data;
         }
     }
 }
