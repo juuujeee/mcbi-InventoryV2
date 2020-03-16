03b267eeb6ea8d8765c4be5058a261dcb218760a
Joji <joji.espinoza@gmail.com>
2020-03-16 10:16:57 +0800

    separate every controllers about stocks

---
+++ ERP.Inventory/Models/VendorList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERP.Inventory.Models
+{
+    public class Vendor
+    {
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        
+    }
+
+    public class Branch
+    {
+        public int ID { get; set; }
+
+        public string BranchName { get; set; }
+    }
+
+    public class VendorInfo
+    {
+        public Vendor VendorSupplier { get; set; }
+        
+        public List<Branch> VendorBranch { get; set; }
+    }
+}
